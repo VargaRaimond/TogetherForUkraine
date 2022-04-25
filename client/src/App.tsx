6cd9db7e46021f4a72089c851feb31a018d0c0bf
biancaciuche <bianca.ciuche@softwire.com>
2022-04-25 19:47:46 +0300

    Added app theme.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,40 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material";
 import logo from "./logo.svg";
-import "./App.css";
+
+const appTheme = createTheme({
+  // https://mui.com/customization/palette/
+  // https://coolors.co/13293d-2f97c1-fde74c-baf2e9-bdede0
+  palette: {
+    primary: {
+      main: "#006494",
+      light: "#E8F1F2",
+      dark: "#13293D",
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: 96,
+    },
+  },
+});
 
 function App() {
-  const x = 5;
-  useEffect(() => {
-    const sayHello = async () => {
-      const response = await fetch("/api/hello");
-      const body = await response.json();
-      console.log(body);
-    };
-    console.log(x);
-    sayHello();
-  }, []);
+  // Don't delete!
+  // Example on how to comunicate with backend
+  // const x = 5;
+  // useEffect(() => {
+  //   const sayHello = async () => {
+  //     const response = await fetch("/api/hello");
+  //     const body = await response.json();
+  //     console.log(body);
+  //   };
+  //   console.log(x);
+  //   sayHello();
+  // }, []);
 
   return (
-    <div className="App">
+    <ThemeProvider theme={appTheme}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -30,7 +49,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </ThemeProvider>
   );
 }
 
