f94baa1e556a580b33e7a2aa9301720f3263e2d1
biancaciuche <bianca.ciuche@softwire.com>
2022-04-26 23:09:26 +0300

    Initial modal

---
+++ client/src/components/pages/get-help/GetHelpModal.tsx
@@ -0,0 +1,100 @@
+import { useMemo } from "react";
+import {
+  Backdrop,
+  Box,
+  Button,
+  Modal,
+  styled,
+  Typography,
+} from "@mui/material";
+import { Delete, Send } from "@mui/icons-material";
+import { IOfferEntry } from "./GetHelpPage";
+
+const boxStyle = {
+  display: "flex",
+  flexDirection: "column",
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxWidth: 440,
+  width: "75%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const SendButton = styled(Button)(({ theme }) => ({
+  margin: "5px",
+  alignSelf: "end",
+  width: "30%",
+  backgroundColor: theme.palette.primary.light,
+}));
+
+const DeleteButton = styled(Button)(({ theme }) => ({
+  margin: "5px",
+  alignSelf: "end",
+  width: "30%",
+  backgroundColor: theme.palette.error.main,
+  "&.MuiButton-text": {
+    color: "white",
+    "&:hover": {
+      color: "inherit",
+    },
+  },
+}));
+
+const GetHelpModal = ({
+  handleClose,
+  offer,
+}: {
+  handleClose: (open: boolean) => void;
+  offer?: IOfferEntry;
+}) => {
+  const open = useMemo(() => !!offer, [offer]);
+
+  return (
+    <Modal
+      aria-labelledby="offer-modal"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        sx: { backgroundColor: "rgba(0,45,65,0.33)" },
+        timeout: 500,
+      }}
+    >
+      <Box sx={boxStyle}>
+        <div style={{ display: "flex", alignItems: "end" }}>
+          <Typography variant="h4" sx={{ margin: "10px 5px 0px 25px" }}>
+            {offer?.name}
+          </Typography>
+
+          <Typography variant="h6" sx={{ margin: "0 15px 0" }}>
+            {offer?.location}
+          </Typography>
+        </div>
+
+        <Typography variant="body1" sx={{ margin: "10px" }}>
+          {offer?.description}
+        </Typography>
+
+        <Typography variant="body2" sx={{ margin: "10px" }}>
+          Remaining: {offer?.remainingOffers} offers
+        </Typography>
+
+        {/* TODO role: only for refugees */}
+        {/* TODO: add onClick */}
+        <SendButton endIcon={<Send />}>Apply now</SendButton>
+
+        {/* TODO role: only for admin */}
+        {/* TODO: add onClick */}
+        <DeleteButton startIcon={<Delete />}>Delete</DeleteButton>
+      </Box>
+    </Modal>
+  );
+};
+
+export default GetHelpModal;
--- client/src/components/pages/get-help/GetHelpPage.tsx
+++ client/src/components/pages/get-help/GetHelpPage.tsx
@@ -2,67 +2,89 @@ import React, { useMemo, useState } from "react";
 import GenericTable from "../../utils/table/GenericTable";
 import { Button, styled } from "@mui/material";
 import GetHelpTableToolbar from "./GetHelpTableToolbar";
+import GetHelpModal from "./GetHelpModal";
 
 const staticRows: IOfferEntry[] = [
   {
     name: "Help 1",
+    description:
+      "Volunteers are individuals who freely offer their time, labor, and expertise. Volunteers often work for schools or NGOs, where they typically receive training and report to designated senior staff. Completely free trial, no card required.",
     volunteer: "Eu",
     category: "Food",
     location: "Bacau",
+    remainingOffers: 5,
   },
   {
     name: "Help 11",
+    description: "",
     volunteer: "Eu",
     category: "Food",
     location: "Bacau",
+    remainingOffers: 5,
   },
   {
     name: "Help 111",
+    description: "",
     volunteer: "Eu",
     category: "Money",
     location: "Bacau",
+    remainingOffers: 5,
   },
   {
     name: "Help 2",
+    description: "",
     volunteer: "Tu",
     category: "Money",
     location: "Bucuresti",
+    remainingOffers: 5,
   },
   {
     name: "Help 3",
+    description: "",
     volunteer: "Noi",
     category: "Money",
     location: "Suceava",
+    remainingOffers: 5,
   },
   {
     name: "Help 4",
+    description: "",
     volunteer: "Voi",
     category: "Money",
     location: "Brasov",
+    remainingOffers: 5,
   },
   {
     name: "Help 5",
+    description: "",
     volunteer: "El",
     category: "Money",
     location: "Bucuresti",
+    remainingOffers: 5,
   },
   {
     name: "Help 6",
+    description: "",
     volunteer: "Ea",
     category: "Money",
     location: "Craiova",
+    remainingOffers: 5,
   },
   {
     name: "Help 7",
+    description: "",
     volunteer: "Ele",
     category: "Food",
     location: "Constanta",
+    remainingOffers: 5,
   },
   {
     name: "Help 8",
+    description: "",
     volunteer: "Ei",
     category: "Accomodation",
     location: "Cluj",
+    remainingOffers: 5,
   },
 ];
 
@@ -78,11 +100,17 @@ const PageWrapper = styled("div")(() => ({
   padding: "25px",
 }));
 
-interface IOfferEntry {
+const SeeMoreButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.light,
+}));
+
+export interface IOfferEntry {
   name: string;
+  description: string;
   volunteer: string;
   category: string;
   location: string;
+  remainingOffers: number;
 }
 
 export interface IOfferTableEntry extends IOfferEntry {
@@ -90,26 +118,33 @@ export interface IOfferTableEntry extends IOfferEntry {
 }
 
 const GetHelpPage = () => {
-  const initialRows = useMemo(() => {
-    return staticRows.map((row) => ({
-      ...row,
-      seeMore: (
-        <Button
-          sx={{ backgroundColor: (theme) => theme.palette.primary.light }}
-          // todo: open modal onClick={() => {}} with info based on current row
-        >
-          See more
-        </Button>
-      ),
-    }));
-  }, []);
+  const [modalOffer, setModalOffer] = useState<IOfferEntry | undefined>(
+    undefined
+  );
 
-  const [offers, setOffers] = useState(initialRows);
+  const initialOffers = useMemo(
+    () =>
+      staticRows.map((offer) => ({
+        ...offer,
+        seeMore: (
+          <SeeMoreButton onClick={() => setModalOffer(offer)}>
+            See more
+          </SeeMoreButton>
+        ),
+      })),
+    []
+  );
+
+  const [offers, setOffers] = useState(initialOffers);
 
   return (
     <PageWrapper>
-      <GetHelpTableToolbar offers={initialRows} setOffers={setOffers} />
+      <GetHelpTableToolbar offers={initialOffers} setOffers={setOffers} />
       <GenericTable rows={offers} headCells={headCells} />
+      <GetHelpModal
+        offer={modalOffer}
+        handleClose={() => setModalOffer(undefined)}
+      />
     </PageWrapper>
   );
 };
