c83d6e8020053f5d05ffd0e49368d07993ef8201
biancaciuche <bianca.ciuche@softwire.com>
2022-05-06 22:53:35 +0300

    Fallback for .env files :(

--- client/src/AuthProvider.tsx
+++ client/src/AuthProvider.tsx
@@ -1,9 +1,12 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const AuthProvider = ({ children }: { children: JSX.Element }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN || "";
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || "";
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE || "";
+  const domain =
+    process.env.REACT_APP_AUTH0_DOMAIN || "dev-gq9lbjoe.us.auth0.com";
+  const clientId =
+    process.env.REACT_APP_AUTH0_CLIENT_ID || "aHKqjQqI3vhJRTI7qX68JxqRw5qQcxwC";
+  const audience =
+    process.env.REACT_APP_AUTH0_AUDIENCE || "http://together-for-ukraine";
 
   return (
     <Auth0Provider
