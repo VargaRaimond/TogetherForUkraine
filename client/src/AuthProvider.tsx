791700f3ebc58970b9b3e54adc68fbd586368aa8
biancaciuche <bianca.ciuche@softwire.com>
2022-05-06 21:48:42 +0300

    Added audience for auth provider

--- client/src/AuthProvider.tsx
+++ client/src/AuthProvider.tsx
@@ -3,13 +3,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN || "";
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || "";
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE || "";
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-      audience="http://together-for-ukraine"
+      audience={audience}
     >
       {children}
     </Auth0Provider>
