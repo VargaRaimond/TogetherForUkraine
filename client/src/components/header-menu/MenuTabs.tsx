09d173473ad26431a06137362302ffc72e77d8e0
biancaciuche <bianca.ciuche@softwire.com>
2022-05-07 11:05:29 +0300

    Conditionally display/hide pages based on roles

--- client/src/components/header-menu/MenuTabs.tsx
+++ client/src/components/header-menu/MenuTabs.tsx
@@ -1,24 +1,42 @@
 import * as React from "react";
 import SmallScreenMenuTabs from "./SmallScreenMenuTabs";
 import LargeScreenMenuTabs from "./LargeScreenMenuTabs";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMemo } from "react";
 
 export interface ILink {
   name: string;
   pathTo: string;
+  roles?: string[]; // If it's undefined, it can be seen by any role
 }
 
-// TODO: Conditionally display / hide some tabs
-const pages: ILink[] = [
+const allPages: ILink[] = [
   { name: "Home", pathTo: "/" },
   { name: "Get help", pathTo: "/get-help" },
   { name: "Provide help", pathTo: "/provide-help" },
-  { name: "Help offers", pathTo: "/help-offers" },
+  { name: "Help offers", pathTo: "/help-offers", roles: ["admin"] },
 ];
 
 const MenuTabs = () => {
+  const { user } = useAuth0();
+
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
+
+  const pages = useMemo(() => {
+    const namespace = "https://localhost:5001";
+    const userRoles: any[] = user ? user[`${namespace}/roles`] : [];
+
+    return allPages.filter(
+      ({ roles: pageRoles }) =>
+        pageRoles === undefined ||
+        pageRoles.some((pageRole) =>
+          userRoles.some((userRole) => userRole === pageRole)
+        )
+    );
+  }, [user]);
+
   return (
     <>
       <SmallScreenMenuTabs
