2b36b0a1ffd93ba2a62f7579cbe9af44be8b3544
biancaciuche <bianca.ciuche@softwire.com>
2022-05-11 22:46:10 +0300

    Updated personServices.ts

--- server/src/services/personServices.ts
+++ server/src/services/personServices.ts
@@ -2,11 +2,11 @@ import { pg } from "../db/knex";
 import { convertPersonDbToApi, IPerson } from "../models/peopleModels";
 
 export async function getPersonNameIfNotAnon(id: string): Promise<string> {
-  const person = await pg("people").where({ id }).select("*");
-  if (person.pop().is_anonymous) {
+  const person = (await pg("people").where({ id }).select("*")).pop();
+  if (person.is_anonymous) {
     return "anonymous";
   }
-  return person.pop().name;
+  return person.name;
 }
 
 export async function getPersonFromId(id: string): Promise<IPerson> {
