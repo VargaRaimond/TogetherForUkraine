bbc906ac56f28491fad88baed686f65261fbe8f7
biancaciuche <bianca.ciuche@softwire.com>
2022-05-12 18:01:37 +0300

    Fetch auth access token

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -11,31 +11,42 @@ router.post(
 );
 
 // TODO: AUTH controller
-
 const setUserRole = async (authId, { role }: { role: string }, res) => {
-  // TODO: maybe add this in the env file?
-  const domain = "dev-gq9lbjoe.us.auth0.com";
-
-  // TODO: token from auth api management (or smth) -> fetch this (24h)
-  // https://manage.auth0.com/dashboard/us/dev-gq9lbjoe/apis/management/explorer
-  const token =
-    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhrSEVPd2hGdGFiZTdGRmdZWHBUSyJ9.eyJpc3MiOiJodHRwczovL2Rldi1ncTlsYmpvZS51cy5hdXRoMC5jb20vIiwic3ViIjoiZWJBSlM1SWZKaDlFdU1YcVdkWFhrc2hBemNGdFVTNDZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LWdxOWxiam9lLnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjUyMzYzMjM0LCJleHAiOjE2NTI0NDk2MzQsImF6cCI6ImViQUpTNUlmSmg5RXVNWHFXZFhYa3NoQXpjRnRVUzQ2Iiwic2NvcGUiOiJyZWFkOmNsaWVudF9ncmFudHMgY3JlYXRlOmNsaWVudF9ncmFudHMgZGVsZXRlOmNsaWVudF9ncmFudHMgdXBkYXRlOmNsaWVudF9ncmFudHMgcmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgZGVsZXRlOnVzZXJzIGNyZWF0ZTp1c2VycyByZWFkOnVzZXJzX2FwcF9tZXRhZGF0YSB1cGRhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGRlbGV0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSByZWFkOnVzZXJfY3VzdG9tX2Jsb2NrcyBjcmVhdGU6dXNlcl9jdXN0b21fYmxvY2tzIGRlbGV0ZTp1c2VyX2N1c3RvbV9ibG9ja3MgY3JlYXRlOnVzZXJfdGlja2V0cyByZWFkOmNsaWVudHMgdXBkYXRlOmNsaWVudHMgZGVsZXRlOmNsaWVudHMgY3JlYXRlOmNsaWVudHMgcmVhZDpjbGllbnRfa2V5cyB1cGRhdGU6Y2xpZW50X2tleXMgZGVsZXRlOmNsaWVudF9rZXlzIGNyZWF0ZTpjbGllbnRfa2V5cyByZWFkOmNvbm5lY3Rpb25zIHVwZGF0ZTpjb25uZWN0aW9ucyBkZWxldGU6Y29ubmVjdGlvbnMgY3JlYXRlOmNvbm5lY3Rpb25zIHJlYWQ6cmVzb3VyY2Vfc2VydmVycyB1cGRhdGU6cmVzb3VyY2Vfc2VydmVycyBkZWxldGU6cmVzb3VyY2Vfc2VydmVycyBjcmVhdGU6cmVzb3VyY2Vfc2VydmVycyByZWFkOmRldmljZV9jcmVkZW50aWFscyB1cGRhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpkZXZpY2VfY3JlZGVudGlhbHMgY3JlYXRlOmRldmljZV9jcmVkZW50aWFscyByZWFkOnJ1bGVzIHVwZGF0ZTpydWxlcyBkZWxldGU6cnVsZXMgY3JlYXRlOnJ1bGVzIHJlYWQ6cnVsZXNfY29uZmlncyB1cGRhdGU6cnVsZXNfY29uZmlncyBkZWxldGU6cnVsZXNfY29uZmlncyByZWFkOmhvb2tzIHVwZGF0ZTpob29rcyBkZWxldGU6aG9va3MgY3JlYXRlOmhvb2tzIHJlYWQ6YWN0aW9ucyB1cGRhdGU6YWN0aW9ucyBkZWxldGU6YWN0aW9ucyBjcmVhdGU6YWN0aW9ucyByZWFkOmVtYWlsX3Byb3ZpZGVyIHVwZGF0ZTplbWFpbF9wcm92aWRlciBkZWxldGU6ZW1haWxfcHJvdmlkZXIgY3JlYXRlOmVtYWlsX3Byb3ZpZGVyIGJsYWNrbGlzdDp0b2tlbnMgcmVhZDpzdGF0cyByZWFkOmluc2lnaHRzIHJlYWQ6dGVuYW50X3NldHRpbmdzIHVwZGF0ZTp0ZW5hbnRfc2V0dGluZ3MgcmVhZDpsb2dzIHJlYWQ6bG9nc191c2VycyByZWFkOnNoaWVsZHMgY3JlYXRlOnNoaWVsZHMgdXBkYXRlOnNoaWVsZHMgZGVsZXRlOnNoaWVsZHMgcmVhZDphbm9tYWx5X2Jsb2NrcyBkZWxldGU6YW5vbWFseV9ibG9ja3MgdXBkYXRlOnRyaWdnZXJzIHJlYWQ6dHJpZ2dlcnMgcmVhZDpncmFudHMgZGVsZXRlOmdyYW50cyByZWFkOmd1YXJkaWFuX2ZhY3RvcnMgdXBkYXRlOmd1YXJkaWFuX2ZhY3RvcnMgcmVhZDpndWFyZGlhbl9lbnJvbGxtZW50cyBkZWxldGU6Z3VhcmRpYW5fZW5yb2xsbWVudHMgY3JlYXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRfdGlja2V0cyByZWFkOnVzZXJfaWRwX3Rva2VucyBjcmVhdGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiBkZWxldGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiByZWFkOmN1c3RvbV9kb21haW5zIGRlbGV0ZTpjdXN0b21fZG9tYWlucyBjcmVhdGU6Y3VzdG9tX2RvbWFpbnMgdXBkYXRlOmN1c3RvbV9kb21haW5zIHJlYWQ6ZW1haWxfdGVtcGxhdGVzIGNyZWF0ZTplbWFpbF90ZW1wbGF0ZXMgdXBkYXRlOmVtYWlsX3RlbXBsYXRlcyByZWFkOm1mYV9wb2xpY2llcyB1cGRhdGU6bWZhX3BvbGljaWVzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMgcmVhZDpwcm9tcHRzIHVwZGF0ZTpwcm9tcHRzIHJlYWQ6YnJhbmRpbmcgdXBkYXRlOmJyYW5kaW5nIGRlbGV0ZTpicmFuZGluZyByZWFkOmxvZ19zdHJlYW1zIGNyZWF0ZTpsb2dfc3RyZWFtcyBkZWxldGU6bG9nX3N0cmVhbXMgdXBkYXRlOmxvZ19zdHJlYW1zIGNyZWF0ZTpzaWduaW5nX2tleXMgcmVhZDpzaWduaW5nX2tleXMgdXBkYXRlOnNpZ25pbmdfa2V5cyByZWFkOmxpbWl0cyB1cGRhdGU6bGltaXRzIGNyZWF0ZTpyb2xlX21lbWJlcnMgcmVhZDpyb2xlX21lbWJlcnMgZGVsZXRlOnJvbGVfbWVtYmVycyByZWFkOmVudGl0bGVtZW50cyByZWFkOmF0dGFja19wcm90ZWN0aW9uIHVwZGF0ZTphdHRhY2tfcHJvdGVjdGlvbiByZWFkOm9yZ2FuaXphdGlvbnNfc3VtbWFyeSByZWFkOm9yZ2FuaXphdGlvbnMgdXBkYXRlOm9yZ2FuaXphdGlvbnMgY3JlYXRlOm9yZ2FuaXphdGlvbnMgZGVsZXRlOm9yZ2FuaXphdGlvbnMgY3JlYXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJzIHJlYWQ6b3JnYW5pemF0aW9uX21lbWJlcnMgZGVsZXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJzIGNyZWF0ZTpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgcmVhZDpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgdXBkYXRlOm9yZ2FuaXphdGlvbl9jb25uZWN0aW9ucyBkZWxldGU6b3JnYW5pemF0aW9uX2Nvbm5lY3Rpb25zIGNyZWF0ZTpvcmdhbml6YXRpb25fbWVtYmVyX3JvbGVzIHJlYWQ6b3JnYW5pemF0aW9uX21lbWJlcl9yb2xlcyBkZWxldGU6b3JnYW5pemF0aW9uX21lbWJlcl9yb2xlcyBjcmVhdGU6b3JnYW5pemF0aW9uX2ludml0YXRpb25zIHJlYWQ6b3JnYW5pemF0aW9uX2ludml0YXRpb25zIGRlbGV0ZTpvcmdhbml6YXRpb25faW52aXRhdGlvbnMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.T5GT5TpOdN27bM4wo5rsZBgDfSrQDThdeEvLPbYB-b5dcxG2vSaWGNeobRW9TgvexThYV3tp4MObYSzNjYXN3_Ohe7oZlinjQTN4u11e_7CbfVSmJOegYx4InSdG8O4AZL7-GHgy4tQbH8kro_TOnc1XBn3cXgh4dY_lpDNOUN08qXWtoFoDcOpox8OoOYiGLDrtlMNfrTPKjvzNO1BbhTANreymuCjwkMweAXz-gVShMSVyEVfU02rXORzTYEYCtazALcaZfwJYIPkrDg2MZdKEz3qWOdTEk1bpocBp0gaEvjgN_sYcDY817AjN7CBc-O1ZN57N3tWjqM96us_RtA";
-
-  // Tutorial on assigning roles: https://auth0.com/docs/manage-users/access-control/configure-core-rbac/rbac-users/assign-roles-to-users
-  const options = {
-    method: "POST",
-    url: `https://${domain}/api/v2/users/${authId}/roles`,
-    headers: {
-      "content-type": "application/json",
-      authorization: `Bearer ${token}`,
-      "cache-control": "no-cache",
-    },
-    data: { roles: [role] },
-  };
-
   try {
-    const response = await axios.request(options);
-    console.log(response.data);
+    // TODO: maybe add this in the env file?
+    const domain = "dev-gq9lbjoe.us.auth0.com";
+    const clientId = "ebAJS5IfJh9EuMXqWdXXkshAzcFtUS46";
+    const clientSecret =
+      "VIgqwr-THYluRtcb_yt7jjPIF3C9QRJDHO4gtRDdjp_SwxPW-edU17UIzDR4e0jV";
+    const audience = "https://dev-gq9lbjoe.us.auth0.com/api/v2/";
+
+    // Tutorial for how to get access token: https://manage.auth0.com/dashboard/us/dev-gq9lbjoe/apis/management/test
+    const accessToken = (
+      await axios.post(
+        `https://${domain}/oauth/token`,
+        {
+          audience,
+          client_id: clientId,
+          client_secret: clientSecret,
+          grant_type: "client_credentials",
+        },
+        { headers: { "content-type": "application/json" } }
+      )
+    )?.data?.access_token;
+
+    // Tutorial for assigning roles: https://auth0.com/docs/manage-users/access-control/configure-core-rbac/rbac-users/assign-roles-to-users
+    await axios.post(
+      `https://${domain}/api/v2/users/${authId}/roles`,
+      { roles: [role] },
+      {
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${accessToken}`,
+          "cache-control": "no-cache",
+        },
+      }
+    );
+
     res.status(201);
   } catch (error) {
     res.status(400);
