fb9234a893069def7240f93a169ee60118615447
biancaciuche <bianca.ciuche@softwire.com>
2022-05-13 21:22:41 +0300

    Added "after submit" behaviour.

--- client/src/components/pages/provide-help/ProvideHelpPage.tsx
+++ client/src/components/pages/provide-help/ProvideHelpPage.tsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { PageWrapper } from "../../utils/CommonComponents";
 import ProvideHelpForm from "./ProvideHelpForm";
 import ProvideHelpNotAuthenticated from "./ProvideHelpNotAuthenticated";
 import LoadingScreen from "../../utils/LoadingScreen";
+import ProvideHelpAfterSubmission from "./ProvideHelpAfterSubmission";
 
 const ProvideHelpPage = () => {
   const { isAuthenticated, isLoading } = useAuth0();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const topPageRef = useRef(null);
 
   if (isLoading) {
     return <LoadingScreen />;
   }
 
   return (
-    <div>
+    <div ref={topPageRef}>
       {!isAuthenticated && <ProvideHelpNotAuthenticated />}
+      {isSubmitted && (
+        <ProvideHelpAfterSubmission setIsSubmitted={setIsSubmitted} />
+      )}
+
       <PageWrapper>
-        <ProvideHelpForm />
+        <ProvideHelpForm
+          disabled={!isAuthenticated || isSubmitted}
+          setIsSubmitted={(value) => {
+            setIsSubmitted(value);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        />
       </PageWrapper>
     </div>
   );
