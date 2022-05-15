aadc57f842faca77d3fda9a3c7447d3c0c28999f
biancaciuche <bianca.ciuche@softwire.com>
2022-05-15 13:08:06 +0300

    RabbitMQ swarm issue fix

--- email-queue-consumer/index.ts
+++ email-queue-consumer/index.ts
@@ -11,22 +11,28 @@ setTimeout(() => {
   const connection = amqplib.connect("amqp://rabbitmq");
 
   connection.then(async (conn) => {
-    const channel = await conn.createChannel();
-    await channel.assertExchange(EXCHANGE_NAME, EXCHANGE_TYPE);
-    await channel.assertQueue(QUEUE_NAME);
-    channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, KEY);
-    channel.consume(QUEUE_NAME, (m) => {
-      const message = JSON.parse(m.content);
-      const { emailContact, messageType, ...messageBody } = message;
+    try {
+      const channel = await conn.createChannel();
+      await channel.assertExchange(EXCHANGE_NAME, EXCHANGE_TYPE);
+      await channel.assertQueue(QUEUE_NAME);
+      channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, KEY);
+      channel.consume(QUEUE_NAME, (m) => {
+        const message = JSON.parse(m.content);
+        const { emailContact, messageType, ...messageBody } = message;
 
-      if (emailContact && messageType && messageBody) {
-        sendEmail(emailContact, messageType, messageBody);
-      } else {
-        // tslint:disable-next-line:no-console
-        console.log("Missing parameters");
-      }
+        if (emailContact && messageType && messageBody) {
+          sendEmail(emailContact, messageType, messageBody);
+        } else {
+          // tslint:disable-next-line:no-console
+          console.log("Missing parameters");
+        }
 
-      channel.ack(m);
-    });
+        channel.ack(m);
+      });
+    } catch (e) {
+      // tslint:disable-next-line:no-console
+      console.log("Connection error:", e);
+      process.exit(-1);
+    }
   });
-}, 10000)
+}, 10000);
